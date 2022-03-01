dea590ecc956491cf5735dd647cda1ccb470a2af
shenhaibo <ayst.shen@foxmail.com>
2022-03-01 17:16:43 +0800

    支持全量测试

--- app/src/main/java/com/ayst/factorytest/MainActivity.java
+++ app/src/main/java/com/ayst/factorytest/MainActivity.java
@@ -1,13 +1,15 @@
 package com.ayst.factorytest;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.ayst.factorytest.adapter.TestItemAdapter;
 import com.ayst.factorytest.base.BaseActivity;
@@ -32,11 +34,16 @@ import butterknife.ButterKnife;
 public class MainActivity extends BaseActivity {
     private static final String TAG = "MainActivity";
 
+    private static final int INVALID_ITEM = -1;
+
     @BindView(R.id.title_bar)
     TitleBar mTitleBar;
     @BindView(R.id.rv_items)
     RecyclerView mItemsRv;
+    @BindView(R.id.btn_start)
+    Button mStartBtn;
 
+    private int mNextItem = INVALID_ITEM;
     private ArrayList<TestItem> mTestItems;
     private TestItemAdapter mTestItemAdapter;
 
@@ -65,10 +72,21 @@ public class MainActivity extends BaseActivity {
         mTestItemAdapter.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(@NonNull BaseQuickAdapter<?, ?> adapter, @NonNull View view, int position) {
+                if (mNextItem != INVALID_ITEM) {
+                    mNextItem = INVALID_ITEM;
+                }
                 Intent intent = new Intent(MainActivity.this,
                         ((TestItem) adapter.getData().get(position)).getActivity());
                 intent.putExtra("item", (TestItem) adapter.getData().get(position));
                 startActivity(intent);
+            }
+        });
+
+        mStartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mNextItem = 0;
+                next();
             }
         });
     }
@@ -84,11 +102,30 @@ public class MainActivity extends BaseActivity {
     public void onResultEvent(ResultEvent event) {
         TestItem item = event.getTestItem();
         Log.i(TAG, "onResultEvent, " + item.toString());
-        for (int i=0; i<mTestItems.size(); i++) {
+        for (int i = 0; i < mTestItems.size(); i++) {
             if (TextUtils.equals(item.getName(), mTestItems.get(i).getName())) {
                 mTestItems.get(i).setState(item.getState());
                 mTestItemAdapter.notifyDataSetChanged();
             }
+        }
+        if (mNextItem != INVALID_ITEM) {
+            next();
+        }
+    }
+
+    private void next() {
+        if (mNextItem < mTestItems.size()) {
+            TestItem item = mTestItems.get(mNextItem);
+
+            Log.i(TAG, "next, test: " + item.getName());
+
+            Intent intent = new Intent(MainActivity.this, item.getActivity());
+            intent.putExtra("item", item);
+            startActivity(intent);
+
+            mNextItem++;
+        } else {
+            mNextItem = INVALID_ITEM;
         }
     }
 }
